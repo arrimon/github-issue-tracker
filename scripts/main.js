97e17d24c8f5898dc734eb8249ac34e165e23a1c
arrimon <rsnrimon17@gmail.com>
2026-03-10 23:40:31 +0600

    Login page successfully done with functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log('Hello from JS')
+document.getElementById('login-btn')
+    .addEventListener('click', () =>{
+            const inputName = document.getElementById('user-name').value;
+            console.log(inputName);
+            
+            const inputPassword = document.getElementById('user-password').value;
+            console.log(inputPassword);
+
+            if(inputName == 'admin' && inputPassword == 'admin123'){
+                alert('Loging Success');
+                window.location.assign('main.html')
+            }else{
+                alert('Please input Correct Username & Password');
+            }
+
+        })
